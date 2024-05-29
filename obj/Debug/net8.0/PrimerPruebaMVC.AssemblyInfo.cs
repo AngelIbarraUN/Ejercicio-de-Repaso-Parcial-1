393e58b384ce482b94d24b3d3bc3f8f5cb77b1e9
Angel Ibarra <al44361@un.edu.mx>
2024-05-29 00:37:53 -0600

    Actualización de Github

--- obj/Debug/net8.0/PrimerPruebaMVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/PrimerPruebaMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimerPruebaMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd906888e76112a8e820e3c3a8b013367807aae5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84a29e2a47bbd4a51991c95004ddea0e5e5c53bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimerPruebaMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimerPruebaMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
